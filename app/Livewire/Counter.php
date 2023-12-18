063522e63f30bcb0d04253ea33b56a5a7bfc8d95
José Divino <josedivinooficial02@gmail.com>
2023-12-18 08:17:03 -0300

    lock property from being authenticated by the frontend

--- app/Livewire/Counter.php
+++ app/Livewire/Counter.php
@@ -2,12 +2,14 @@
 
 namespace App\Livewire;
 
+use Livewire\Attributes\Locked;
 use Livewire\Component;
 
 class Counter extends Component
 {
+    #[Locked]
     public int $counter = 0;
-    public string $name = '';
+    public string $name = 'Lais';
     public string $lastName = '';
 
     public function mount()
@@ -27,6 +29,6 @@ class Counter extends Component
 
     public function refresh()
     {
-
+        $this->reset(['name', 'lastName']);
     }
 }
--- resources/views/livewire/counter.blade.php
+++ resources/views/livewire/counter.blade.php
@@ -6,6 +6,11 @@
     <x-primary-button wire:click="refresh">Refresh</x-primary-button>
 
     <br><br>
+    <div x-data="">
+        <span x-text="$wire.name"></span>
+        <x-secondary-button @click="$wire.set('name', 'Divino')">Test</x-secondary-button>
+    </div>
+    <br><br>
 
     Name:: {{ $name }} {{ $lastName }}
 </div>
